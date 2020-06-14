bd5be3a7e0f4439c4cbf538020bdc51561df310f
doldam0 <jinustar@g.skku.edu>
2020-06-14 22:46:16 +0900

    [Edit] Remove debugging string

--- src/com/hextortoise/Main.java
+++ src/com/hextortoise/Main.java
@@ -4,7 +4,6 @@ import com.hextortoise.frames.MainFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(Util.RESOURCES_PATH);
 		MainFrame mainFrame = new MainFrame();
 	}
 }
